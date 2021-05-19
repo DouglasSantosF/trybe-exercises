9c5691c81832e8be29108ab769d300af2c7afa9b
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-05-19 17:16:57 -0300

    exercicios de conteúdos

---
+++ bloco_4/exercicio_cont/script.js
@@ -0,0 +1,20 @@
+const paragraph = document.getElementById("paragraph");
+paragraph.style.color = "red";
+
+/* const mudaTitle = document.getElementById("page-title");
+mudaTitle = "Oii"; */
+ document.getElementById("page-title").innerText = "Nosso Lar";
+
+ document.getElementById("second-paragraph").innerText = " Uma vida pode ter mais de um significado!";
+
+ document.getElementById("subtitle").innerText = " Um filme que nos trás muitas lições!!";
+
+ 
+
+
+document.getElementsByClassName("parag")[0].style.color = "green";
+document.getElementsByClassName("parag")[1].style.color = "green";
+
+ 
+
+document.getElementsByTagName("h2")[0].style.color = "blue";
