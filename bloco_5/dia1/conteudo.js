7577f79b988528f45f56838a1d282906088fa3e5
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-05-26 20:25:33 -0300

    bloco 5 iniciado

---
+++ bloco_5/dia1/conteudo.js
@@ -0,0 +1,15 @@
+
+ const paragraph = document.getElementById("paragraph");
+ paragraph.style.color = "red"; 
+
+ document.getElementById("page-title").innerText = "Nosso Lar" ;
+
+ document.getElementById("second-paragraph").innerText = "Nossa primeira aula do Bloco 5";
+
+ document.getElementById("subtitle").innerHTML = "#vqv";
+
+ document.getElementsByClassName("para");
+
+ document.getElementsByClassName("para")[0].style.color = "blue";
+
+ document.getElementsByTagName("h2")[0].style.border = "solid 1px green";
