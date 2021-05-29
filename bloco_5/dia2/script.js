4d51919291d6e84e86a911a8284766b7c4de3bc8
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-05-29 20:37:41 -0300

    imagem adicionada

--- bloco_5/dia2/script.js
+++ bloco_5/dia2/script.js
@@ -1,10 +1,31 @@
-a = document.createElement("div");
+let a = document.createElement("div");
 document.body.appendChild(a);
 a.className="main-content"; 
 
-b = document.createElement("h1");
+let b = document.createElement("h1");
 document.body.appendChild(b);
 b.innerText = "Exercício 5.2 - JavaScript DOM ";
+
+let c = document.createElement("div");
+a.appendChild(c);
+c.className = "center-content";
+
+let d = document.createElement("p");
+c.appendChild(d);
+d.innerText = "SabadouuuL";
+
+let e = document.createElement("div");
+a.appendChild(e);
+e.className = "left-content";
+
+let f = document.createElement("div");
+a.appendChild(f);
+f.className ="right-content";
+
+let g = document.createElement("img");
+e.appendChild(g);
+g.src ="https://picsum.photos/200";
+g.className = "small-image";
 
 
 console.log(document.body);
