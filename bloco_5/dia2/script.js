e09ba64c266eadcc9caeec39d2185092cc4a0bdc
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-05-29 20:14:24 -0300

    h1 adicionado

--- bloco_5/dia2/script.js
+++ bloco_5/dia2/script.js
@@ -1,6 +1,10 @@
 a = document.createElement("div");
 document.body.appendChild(a);
-
 a.className="main-content"; 
+
+b = document.createElement("h1");
+document.body.appendChild(b);
+b.innerText = "Exercício 5.2 - JavaScript DOM ";
+
 
 console.log(document.body);
