bb2679c8383a1ebb77d057952ed23a7ff32f6aa9
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-10 12:27:16 -0300

    encerrando exercicios bloco 5

--- bloco_5/dia3/exercicios/script.js
+++ bloco_5/dia3/exercicios/script.js
@@ -8,9 +8,25 @@ function createDaysOfTheWeek() {
     dayListItem.innerHTML = days;
 
     weekDaysList.appendChild(dayListItem);
-  };
+  }
+}
+
+
+
+createDaysOfTheWeek(){
+  
+  const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+  const daysList = document.getElementById("days");
+
+  for(let index =0; index < dezDaysList; i+=1){
+    let numDia = dezDaysList[index];
+    
+    if(numDia == )
+
+
+
+  }
+
+
 };
 
-createDaysOfTheWeek();
-
-// Escreva seu código abaixo.
