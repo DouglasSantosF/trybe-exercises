cb46c016bd4cd7ace16db91ac1b9a9ec3edf65ba
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-17 22:03:54 -0300

    exercicios até o 3 parte 2

---
+++ bloco_7/conteudos/arrowF.js
@@ -0,0 +1,8 @@
+const printName = nome => nome = "Douglas dos Santos";
+
+console.log(printName());
+
+const separaPalavras = (frase) =>  frase.split(' ');
+  
+
+console.log(separaPalavras("Douglas dos Santos" ));
---
+++ bloco_7/exercicio-3.js
@@ -0,0 +1,3 @@
+const fatorial = n =>  n = n*(n-1);
+
+console.log(fatorial(5));
