ce717ddf3d8a7d8776efa7ad5de9c062eed51293
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-06-26 22:48:05 -0300

    conteudo 8.2 finalizado

---
+++ bloco_8/dia2/conteudo/sort.js
@@ -0,0 +1,23 @@
+/* const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((a,b) => a.age - b.age);
+
+console.log(people); */
+
+const people = [
+  { name: 'Mateus', age: 18 },
+  { name: 'José', age: 16 },
+  { name: 'Ana', age: 23 },
+  { name: 'Cláudia', age: 20 },
+  { name: 'Bruna', age: 19 },
+];
+
+people.sort((pe1, pe2) => pe2.age - pe1.age);
+
+console.log(people);
