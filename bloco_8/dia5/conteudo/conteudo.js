8bf4825e1acce71cb9e853ef397a644b076623f1
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-07-03 19:09:45 -0300

    conteudo bloco8.5 finalizado

---
+++ bloco_8/dia5/conteudo/conteudo.js
@@ -0,0 +1,25 @@
+//Spread array
+
+/* const numbers = [1, 2, 3];
+
+const newArray = [4, 5, 6,...numbers];
+console.log(newArray); // [ 1, 2, 3, 4, 5, 6 ]
+console.log(numbers); // [ 1, 2, 3 ] */
+
+/* const spring = ['OUT', 'NOV', 'DEZ'];
+const summer = ['JAN', 'FEV', 'MAR'];
+const fall = ['ABR', 'MAI', 'JUN'];
+const winter = ['JUL', 'AGO', 'SET'];
+
+const months = [...summer, ...fall, ...winter, ...spring];
+console.log(months); /* [
+  'JAN', 'FEV', 'MAR',
+  'ABR', 'MAI', 'JUN',
+  'JUL', 'AGO', 'SET',
+  'OUT', 'NOV', 'DEZ'
+] */ 
+
+const randomNumbers = [57, 8, 5, 800, 152, 74, 630, 98, 40];
+
+console.log(Math.max(...randomNumbers)); // 800
+console.log(Math.min(...randomNumbers)); // 5
