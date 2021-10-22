93bb3d7f490cacf4fb8570383686aaa0a3303f24
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-22 19:04:05 -0300

    exercicios 6 e 7 feitos

---
+++ bloco_19/19.1/Exercícios/exercicios/exercicio6.sql
@@ -0,0 +1,3 @@
+/* Qual tipo de relacionamento a tabela country faz com a tabela city ? */
+
+/* Resposta: N : 1 */
---
+++ bloco_19/19.1/Exercícios/exercicios/exercicio7.sql
@@ -0,0 +1,5 @@
+/* Abra tabela por tabela do banco sakila e encontre no mínimo 3 exemplos de um relacionamentos 1:N ou N:1. */
+
+/*Resposta : 1 : N -> filme_id com category_id */
+/*Resposta : 1 : N -> adress_id com city_id */
+/*Resposta : 1 : N -> customer_id com store_id */
