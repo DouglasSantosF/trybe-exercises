3cb776db304e57aacd5c864b1021edd4ee8e8b5d
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-22 18:44:37 -0300

    exercicio 3 feito

---
+++ bloco_19/19.1/Exercícios/exercicios/exercicio3.sql
@@ -0,0 +1,10 @@
+/* 
+  crie uma tabela com as seguintes restrições:
+Nome da tabela: Filme
+Colunas:
+FilmeId - primary key, tipo int, incrementa por 1 cada vez que um valor é inserido automaticamente;
+Descricao - não permite nulos, tipo texto (varchar(100));
+AnoLancamento - não permite nulos, tipo int;
+Nota - permite nulos, tipo int;
+
+ */
