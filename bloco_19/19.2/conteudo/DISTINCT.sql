a652c7404509e483d4c0d85fdda0c09035e8975d
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-23 18:49:31 -0300

    conteúdo CONCAT

---
+++ bloco_19/19.2/conteudo/DISTINCT.sql
@@ -0,0 +1,34 @@
+/* 
+
+Tabela fictícia
+Para criá-la no seu banco de dados, abra uma nova janela de query no MySQL Workbench e execute o seguinte código:
+Copiar
+CREATE DATABASE `Escola`;
+CREATE TABLE IF NOT EXISTS Escola.Alunos (
+    `Nome` VARCHAR(7) CHARACTER SET utf8,
+    `Idade` INT
+);
+INSERT INTO Escola.Alunos VALUES
+    ('Rafael', 25),
+    ('Amanda', 30),
+    ('Roberto', 45),
+    ('Carol', 19),
+    ('Amanda', 25);
+ */
+
+/* Monte uma query para encontrar pares únicos de nomes e idades . */
+  SELECT DISTINCT Nome, Idade FROM Escola.Alunos;
+
+/* Quantas linhas você encontraria na query anterior? */
+  5
+/* Monte uma query para encontrar somente os nomes únicos. */
+  SELECT DISTINCT Nome FROM Escola.Alunos;
+
+/* Quantas linhas você encontraria na query anterior? */
+  4
+
+/* Monte uma query para encontrar somente as idades únicas. */
+  SELECT DISTINCT Idade FROM Escola.Alunos;
+
+/* Quantas linhas você encontraria na query anterior?*/
+  4
