27ab88e9e01e91225afc58e87e8d7bd07865199d
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-23 17:47:34 -0300

    exercicio conteudo SELECT

---
+++ bloco_19/19.2/conteudo/SELECT.sql
@@ -0,0 +1,29 @@
+/* Escreva uma query que selecione todas as colunas da tabela city ; */
+  SELECT * FROM sakila.city;
+
+/* Escreva uma query que exiba apenas as colunas first_name e last_name da tabela customer ; */
+  SELECT first_name, last_name FROM sakila.customer;
+
+/* Escreva uma query que exiba todas as colunas da tabela rental ; */
+  SELECT * FROM sakila.rental;
+
+/* Escreva uma query que exiba o título, a descrição e a data de lançamento dos filmes registrados na tabela film ; */
+  SELECT title, description, release_year FROM sakila.film;
+
+/* Utilize o SELECT para explorar todas as tabelas do banco de dados. */
+  SELECT * FROM sakila.actor ;
+  SELECT * FROM sakila.address ;
+  SELECT * FROM sakila.category ;
+  SELECT * FROM sakila.city ;
+  SELECT * FROM sakila.country ;
+  SELECT * FROM sakila.customer ;
+  SELECT * FROM sakila.film ;
+  SELECT * FROM sakila.filme_actor ;
+  SELECT * FROM sakila.filme_category ;
+  SELECT * FROM sakila.film_text ;
+  SELECT * FROM sakila.inventory ;
+  SELECT * FROM sakila.language ;
+  SELECT * FROM sakila.paymant ;
+  SELECT * FROM sakila.rental ;
+  SELECT * FROM sakila.staff ;
+  SELECT * FROM sakila.store ;
--- bloco_19/19.2/conteudo/praticando.sql
+++ bloco_19/19.2/conteudo/praticando.sql
@@ -1,18 +1,16 @@
-/* 
-Monte uma query que exiba seu nome na tela;
+/* Monte uma query que exiba seu nome na tela; */
   SELECT 'Douglas' AS Nome;
 
-Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela;
+/* Monte uma query que exiba seu nome, sobrenome, cidade natal e idade na tela; */
   SELECT 'Douglas', 'dos Santos Frutuoso', 'Rio de Janeiro', '29' ;
 
-Monte uma query que, além de exibir todas as informações já mencionadas, identifique cada coluna usando o AS , que é chamado de alias na linguagem SQL ( alias é como um apelido no português);
+/* Monte uma query que, além de exibir todas as informações já mencionadas, identifique cada coluna usando o AS , que é chamado de alias na linguagem SQL ( alias é como um apelido no português); */
   SELECT 'Douglas' AS Nome, 'dos Santos Frutuoso' AS Sobrenome, 'Rio de Janeiro' AS 'Cidade Natal', 29 AS Idade;
 
-Qual é o resultado de 13 * 8 ? Descubra usando apenas o SELECT ;
+/* Qual é o resultado de 13 * 8 ? Descubra usando apenas o SELECT ; */
   SELECT 13 * 8;
 
-Monte uma query que exiba a data e hora atuais. Dê a essa coluna o nome "Data Atual".
+/* Monte uma query que exiba a data e hora atuais. Dê a essa coluna o nome "Data Atual". */
   SELECT now() AS 'Data Atual';
 
- */
  
