166c1bce43c478703481556f00e97cff092842c9
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-23 20:54:43 -0300

    att exercicios 9

--- bloco_19/19.2/exercicios/exercicio9.sql
+++ bloco_19/19.2/exercicios/exercicio9.sql
@@ -1,1 +1,2 @@
 /* Escreva uma query que exiba a string "O projeto Name precisou de Hours horas para ser concluído." para cada projeto. */
+  SELECT 'O projeto Name precisou de Hours horas para ser concluído.';
