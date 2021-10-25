cf66e13570a43923550246c595218c65573aa294
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-25 18:20:29 -0300

    LIKE cont 7

--- bloco_19/19.3/conteudo/LIKE.sql
+++ bloco_19/19.3/conteudo/LIKE.sql
@@ -26,3 +26,6 @@
   OR title LIKE 'mosquito%';
 
 /* Mostre os seis filmes que contêm as palavras monkey e sumo em suas descrições. */
+  SELECT * FROM sakila.film
+  WHERE description LIKE '%monkey%'
+  AND description LIKE '%sumo%';
