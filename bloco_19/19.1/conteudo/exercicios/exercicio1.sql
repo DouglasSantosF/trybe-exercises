ef555333b6971f6f2d726ebd82329ab25298cc23
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-25 19:37:57 -0300

    IN BETWEEN cont 3

--- bloco_19/19.1/conteudo/exercicios/exercicio1.sql
+++
@@ -1,5 +0,0 @@
-/* Exercício 1 : Descubra como fazer uma pesquisa em qualquer tabela sem utilizar uma linha de código usando o MySql Workbench .*/
-
-/* Resposta: no menu Schemas a esquerda, clicando em um bando de dados  qualquer, por exemplo no sakila -> Tables -> e depois
- em uma instância com o botão direito do mouse, vão surgir um meni de opções, selecionando a primeira opção Select Rows -
- vai retornar a instância completa com todas as colunas e linhas*/
