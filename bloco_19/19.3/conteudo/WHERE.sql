257459994dae80f93b5271d6efcd021056c0e0c0
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-25 17:45:17 -0300

    WHERE CONT 3 e 4

--- bloco_19/19.3/conteudo/WHERE.sql
+++ bloco_19/19.3/conteudo/WHERE.sql
@@ -20,9 +20,21 @@ As informações podem ser encontradas na tabela customer */
   entre os filmes feitos para menores de idade e que têm o custo mínimo de substituição de $18,00 dólares.
   Em caso de empate, ordene em ordem alfabética pelo título. As informações podem ser encontradas na tabela film */
 
+  SELECT title, description, release_year, replacement_cost FROM sakila.film
+  WHERE replacement_cost >= 18
+  AND rating = 'PG'
+  ORDER BY replacement_cost DESC, title
+  LIMIT 100;
+
 /* Quantos clientes estão ativos e na loja 1 ? As informações podem ser encontradas na tabela customer */
+  SELECT COUNT(*) FROM sakila.customer
+  WHERE active = true
+  AND store_id = 1;
+  /* 318 */
 
 /* Mostre todos os detalhes dos clientes que não estão ativos na loja 1 . As informações podem ser encontradas na tabela customer */
+
+
 /* Precisamos descobrir quais são os 50 filmes feitos apenas para adultos com a menor taxa de aluguel,
 para que possamos fazer uma divulgação melhor desses filmes. Em caso de empate, ordene em ordem alfabética pelo título.
  As informações podem ser encontradas na tabela film */
