249b1e1e6074129c9c65b509625ec677b37dce1b
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-26 13:25:05 -0300

    INSERT cont 2

--- bloco_19/19.4/conteudo/INSERT.sql
+++ bloco_19/19.4/conteudo/INSERT.sql
@@ -1,13 +1,16 @@
 /* Insira um novo funcionário na tabela sakila.staff . */
+/* Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector".*/
   INSERT INTO sakila.staff (first_name, last_name, address_id, email, store_id, active, username)
   VALUES('Douglas', 'Santos', 2,'alguem@alguem.com', 2, 1, 'Doug');
 
-/* Para saber quais campos são obrigatórios, clique com o botão direito na tabela sakila.staff e selecione "Table Inspector".
 
 
 /* Clique na aba "columns" e verifique quais campos aceitam nulos para te guiar.
   Lembre-se de que valores que são gerados automaticamente não precisam ser inseridos manualmente. Boa explorada!*/
-
+    INSERT INTO sakila.staff (first_name, last_name, address_id, email, store_id, active, username)
+    VALUES
+    ('John', 'Snow', 5,'john@stark.com', 2, 1, 'Ghost'),
+    ('Son', 'Goku', 3, 'Goku@corpcapsule.com',1,1, 'kakarotto');
 
 /* Feito o exercício anterior, vamos agora para o nível 2. Insira dois funcionários novos em apenas uma query . */
 
