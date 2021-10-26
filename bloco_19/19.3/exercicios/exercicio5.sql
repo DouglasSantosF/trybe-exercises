6b0086de8146d54dd26da916fc23d88cd6273a84
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-26 10:43:34 -0300

    exercicio 6

--- bloco_19/19.3/exercicios/exercicio5.sql
+++ bloco_19/19.3/exercicios/exercicio5.sql
@@ -1,5 +1,4 @@
-/* Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 .
- Ordene os resultados por ordem crescente. */
+/* Escreva uma query para exibir o número de empresas (fornecedores) que contém a letra F no código. */
   SELECT COUNT(*) FROM PecasFornecedores.Fornecimentos
   WHERE Fornecedor LIKE '%F%';
 
