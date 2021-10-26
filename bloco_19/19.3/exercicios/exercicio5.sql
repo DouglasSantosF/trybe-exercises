837dc5316d2ffc0640d910de2aede952688fdfb4
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-26 10:33:04 -0300

    exercicio 5

---
+++ bloco_19/19.3/exercicios/exercicio5.sql
@@ -0,0 +1,5 @@
+/* Escreva uma query para exibir os fornecimentos onde as peças custam mais de R$15,00 e menos de $40,00 .
+ Ordene os resultados por ordem crescente. */
+  SELECT COUNT(*) FROM PecasFornecedores.Fornecimentos
+  WHERE Fornecedor LIKE '%F%';
+
