bf8cf9845e2477e5c508d9c6f55592fd49d17a70
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-27 11:25:41 -0300

    exercicio 5

--- bloco_19/19.4/exercicio/exercicio2.sql
+++ bloco_19/19.4/exercicio/exercicio2.sql
@@ -1,5 +1,5 @@
   /* Procurando Nemo foi aclamado pela crítica! Foi classificado em 6.8, fez 450 milhões no mercado interno e 370 milhões no mercado internacional.
    Adicione as informações à tabela BoxOffice . */
   
-  INSERT INTO Pixar.BoxOffice(rating, domestic_sales, international_sales)
-  VALUES (6.8, 450000000,370000000);
+  INSERT INTO Pixar.BoxOffice(movie_id, rating, domestic_sales, international_sales)
+  VALUES (9, 6.8, 450000000,370000000);
