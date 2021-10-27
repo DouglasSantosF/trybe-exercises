072af837f310c237e1a2c3c4cbd87ad4d09d23d2
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-27 10:56:49 -0300

    exercicio 3

---
+++ bloco_19/19.4/exercicio/exercicio3.sql
@@ -0,0 +1,9 @@
+/* O diretor do filme "Procurando Nemo" está incorreto, na verdade ele foi dirigido por Andrew Staton.
+ Corrija esse dado utilizando o UPDATE . */
+
+
+SET SQL_SAFE_UPDATES = 0;
+
+UPDATE Pixar.Movies
+SET director = "Andrew Staton"
+WHERE title = "Procurando Nemo";
