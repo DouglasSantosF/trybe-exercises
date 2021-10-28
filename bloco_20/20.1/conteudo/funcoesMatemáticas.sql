dabd2eb77297493cfc35552ce27bc67cc56e2450
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-28 18:20:10 -0300

    math ex6

--- bloco_20/20.1/conteudo/funcoesMatemáticas.sql
+++ bloco_20/20.1/conteudo/funcoesMatemáticas.sql
@@ -26,10 +26,12 @@
 /* Monte uma query que exiba o valor arredondado
  de 15.7515971 com uma precisão de 5 casas decimais. */
   SELECT ROUND( 15.7515971, 5);
+  --15.75160
 
 /* Estamos com uma média de 39.494 de vendas de camisas por mês.
  Qual é o valor aproximado para baixo dessa média? */
-
+  SELECT FLOOR(39.494);
+  --39
 
 /* Temos uma taxa de inscrição de 85.234% no curso de fotografia para iniciantes.
  Qual é o valor aproximado para cima dessa média? */
