12203f3cf88b4a5fbdbfebfee0fcd5539bc32077
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-31 12:56:31 -0300

    RIGHT CONT

--- bloco_20/20.2/conteudo/left_right.sql
+++ bloco_20/20.2/conteudo/left_right.sql
@@ -7,7 +7,21 @@ SELECT
     a.actor_id,
     a.first_name,
     a.last_name
-FROM customer AS c
-LEFT JOIN actor AS a
+FROM sakila.customer AS c
+LEFT JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
+
+-- exemplo de RIGHT 
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+RIGHT JOIN sakila.actor AS a
 ON c.last_name = a.last_name
 ORDER BY c.last_name;
