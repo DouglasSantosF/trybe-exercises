c7876dc1108e8b25587f2e14a2107bb74e22fe57
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-10-31 12:57:11 -0300

    INNER CONT

--- bloco_20/20.2/conteudo/left_right.sql
+++ bloco_20/20.2/conteudo/left_right.sql
@@ -25,3 +25,17 @@ FROM sakila.customer AS c
 RIGHT JOIN sakila.actor AS a
 ON c.last_name = a.last_name
 ORDER BY c.last_name;
+
+-- exemplo INNER JOIN
+
+SELECT
+    c.customer_id,
+    c.first_name,
+    c.last_name,
+    a.actor_id,
+    a.first_name,
+    a.last_name
+FROM sakila.customer AS c
+INNER JOIN sakila.actor AS a
+ON c.last_name = a.last_name
+ORDER BY c.last_name;
