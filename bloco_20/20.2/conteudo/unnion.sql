0c86e61d41790ecd9b267fb0a237faf1442f566c
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-11-01 15:14:49 -0300

    UNION EX2

--- bloco_20/20.2/conteudo/unnion.sql
+++ bloco_20/20.2/conteudo/unnion.sql
@@ -9,7 +9,11 @@
 /* Monte uma query que una os resultados das tabelas customer e actor ,
  exibindo os nomes que contêm a palavra "tracy" na tabela customer e os que contêm "je" na tabela actor .
  Exiba apenas os resultados únicos. */
-
+  SELECT first_name FROM sakila.actor
+  WHERE first_name LIKE '%je%'
+UNION
+  SELECT first_name FROM sakila.customer
+  WHERE first_name LIKE '%tracy%';
 
 
 /* Monte uma query que exiba a união dos cinco últimos nomes da tabela actor ,
