c5e0e2f681ac89a6d7e95effbb67c00d2603eafc
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-11-01 15:06:38 -0300

    UNION EX1

--- bloco_20/20.2/conteudo/self_join.sql
+++ bloco_20/20.2/conteudo/self_join.sql
@@ -9,7 +9,7 @@
  Exiba apenas os filmes com a duração de empréstimo entre 2 e 4 dias. */
   SELECT t1.title, t1.rental_duration, t2.title, t2.rental_duration
   FROM  sakila.film AS t1,
-      sakila.film AS t2
+    sakila.film AS t2
   WHERE t1.rental_duration = t2.rental_duration 
   AND
   t1.rental_duration BETWEEN 2 AND 4; 
