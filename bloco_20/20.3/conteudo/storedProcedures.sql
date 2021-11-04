a1a9893e2363c461781b0acebdce747a9f600cce
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-11-04 14:24:46 -0300

    PROCEDURE ex2

--- bloco_20/20.3/conteudo/storedProcedures.sql
+++ bloco_20/20.3/conteudo/storedProcedures.sql
@@ -22,7 +22,19 @@ a quantidade de filmes em que atuaram. */
 da categoria desejada em uma string e que exiba o id do filme ,
 seu titulo , o id de sua categoria e o nome da categoria selecionada.
 Use as tabelas film , film_category e category para montar essa procedure. */
+  USE sakila;
+  DELIMITER $$
 
+  CREATE PROCEDURE retornaFilmesPelaCategoria( IN categoria VARCHAR(100))
+  BEGIN
+    SELECT f.film_id, f.title, fc.category_id, c.name
+      FROM sakila.film f
+      INNER JOIN sakila.film_category fc ON f.film_id = fc.film_id
+      INNER JOIN sakila.category c ON c.category_id = fc.category_id
+      WHERE c.name = categoria;
+  END $$
+
+  DELIMITER 
 
 
 /* Monte uma procedure que receba o email de um cliente como parâmetro
