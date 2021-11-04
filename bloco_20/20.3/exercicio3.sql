ef20b06db483ecf15bd9a3e4dc9da03e697f0224
Douglas dos Santos <doug.frutuoso@gmail.com>
2021-11-04 18:43:25 -0300

    exercicio 1

---
+++ bloco_20/20.3/exercicio3.sql
@@ -0,0 +1,3 @@
+/* Crie um Trigger na tabela movies que, ao ter algum de seus registros excluídos,
+  deve enviar uma informação para a tabela movies_logs , onde devem ser guardados a data da exclusão,
+  a executed_action 'DELETE' e o id do filme excluído. */
